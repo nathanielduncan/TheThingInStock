0f0731765587e69bca63c7eb73017105fd7fa3a3
nathanielduncan <54687384+nathanielduncan@users.noreply.github.com>
2021-10-29 17:08:29 -0500

    auth.py upload
    All values have been replaced with placeholders. They need to be changed to the security keys for the Twitter account that is going to be used.

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = '1f65sa1df68sdf4g6we5a1df3v'
+consumer_secret     = 'd1f5asd86as4f65as1f32asd1f68as1'
+access_token        = 'sd1f65sad4f6s8dv35sd1f6sa8d4fsd'
+access_token_secret = '5g1fbgfbsdf53g1vdf86asdcf3s21cs86d1vsdv'
