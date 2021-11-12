3e5dd864783376a645c42c6a2e62bb4b47a5dac1
Nathan <54687384+nathanielduncan@users.noreply.github.com>
2021-11-12 15:04:31 -0600

    Switched from Chrome to Firefox
    Everybody likes Firefox more, duh. Needs to have geckodriver.exe, info added to readme. Update to .gitignore for geckodriver.exe.

--- StockBot.py
+++ StockBot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
     #selenium is the API that allows me to open a browser, find a object in the HTML and check its content
 
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
     #webdriver makes it so that the browser version being used by selenium is compatible
 
 from twython import Twython
@@ -34,8 +34,14 @@ url = "https://www.bestbuy.com/site/pokemon-pokemon-tcg-celebrations-ultra-premi
 refreshrate = int(60)
         #refreshrate above can be changed for how often (in seconds) the stauts will be checked.
 
+geckoLocation = 'C:\\Users\crazy\OneDrive\Documents\Coding\StockBot\geckodriver.exe'
+        #geckoLocation should be the file path to the corrsponding geckodriver
+        #download the one for your system here: https://github.com/mozilla/geckodriver/releases
+
+
+
 #open the browser
-browser = webdriver.Chrome(ChromeDriverManager().install())
+browser = webdriver.Firefox(executable_path=geckoLocation)
 browser.get(url)
 
 
--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key        = '1f65sa1df68sdf4g6we5a1df3v'
-consumer_secret     = 'd1f5asd86as4f65as1f32asd1f68as1'
-access_token        = 'sd1f65sad4f6s8dv35sd1f6sa8d4fsd'
-access_token_secret = '5g1fbgfbsdf53g1vdf86asdcf3s21cs86d1vsdv'
+consumer_key        = '581hZzr7II32i7WwtHWXVPmCd'
+consumer_secret     = 'Uq2BerfK24Fo6uIOS5xv191J8d3s8ZSzI2iLgrEPKptPqBbMzb'
+access_token        = '1454182650298699776-ixiboSvm3wzYN2ej8m5KVx86RZ1uKB'
+access_token_secret = 'UOSiXhEmnH80ifp36rj7y9FgXGmyAo4Rp4C2TD8jeVBdI'
