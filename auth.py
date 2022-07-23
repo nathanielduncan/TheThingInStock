ac00c3a2a89e78d51f259b0b8981e5941e6a6455
Nathan <54687384+nathanielduncan@users.noreply.github.com>
2022-07-23 11:41:34 -0500

    Update auth.py
    Removing keys that were never suppose to be uploaded.

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key        = '581hZzr7II32i7WwtHWXVPmCd'
-consumer_secret     = 'Uq2BerfK24Fo6uIOS5xv191J8d3s8ZSzI2iLgrEPKptPqBbMzb'
-access_token        = '1454182650298699776-ixiboSvm3wzYN2ej8m5KVx86RZ1uKB'
-access_token_secret = 'UOSiXhEmnH80ifp36rj7y9FgXGmyAo4Rp4C2TD8jeVBdI'
+consumer_key = 'enterkey'
+consumer_secret = 'enterkey'
+access_token = 'entertoken'
+access_token_secret = 'entertoken'
